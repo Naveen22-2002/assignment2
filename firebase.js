dc51a3f2c2d8410525a934abc75009c1e9633964
Naveen <naveenede4@gmail.com>
2024-06-04 15:28:45 +0530

    first commit

---
+++ firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAMj_3POqaLzrpVWxZBziV7puZTvYPJG8k",
+    authDomain: "movie-application-efdee.firebaseapp.com",
+    projectId: "movie-application-efdee",
+    storageBucket: "movie-application-efdee.appspot.com",
+    messagingSenderId: "1045537215925",
+    appId: "1:1045537215925:web:0b1bc518cbad66ca33f5fc",
+    measurementId: "G-V4M66FVBX2"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+ firebase.analytics();
